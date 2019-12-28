edc18eba7d03bf4d9751281e8bec3801416145c2
Jason Ortiz <jasonortiz2727@gmail.com>
2019-12-28 18:11:59 -0500

    Fixed issue where conflicting annotations prevented application from starting on reactor netty server

--- src/main/java/com/ortiz/userprofilestore/config/ReactiveWebSecurityConfig.java
+++ src/main/java/com/ortiz/userprofilestore/config/ReactiveWebSecurityConfig.java
@@ -3,15 +3,15 @@ package com.ortiz.userprofilestore.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
-@EnableWebSecurity
+@EnableWebFluxSecurity
 @Configuration
 @Profile("default")
-public class WebSecurityConfig {
+public class ReactiveWebSecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
