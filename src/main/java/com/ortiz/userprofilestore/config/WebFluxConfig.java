d5638cbb85e65ea227dc1eb0a8b9d24b4b5d9c60
Jason Ortiz <jasonortiz2727@gmail.com>
2019-12-30 23:36:11 -0500

    Fixed Swagger

--- src/main/java/com/ortiz/userprofilestore/config/WebFluxConfig.java
+++ src/main/java/com/ortiz/userprofilestore/config/WebFluxConfig.java
@@ -4,15 +4,22 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
-import org.springframework.web.reactive.config.DelegatingWebFluxConfiguration;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class WebFluxConfig extends DelegatingWebFluxConfiguration {
+public class WebFluxConfig implements WebFluxConfigurer {
 
     @Override
-    protected void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
         Jackson2JsonEncoder encoder = new Jackson2JsonEncoder();
         encoder.getObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
         configurer.defaultCodecs().jackson2JsonEncoder(encoder);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/documentation/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/documentation/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
